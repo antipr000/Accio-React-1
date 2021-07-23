d7dff451eb0ae3d4fc5aad26d5edcdc7b56b14f0
Soham Mukherjee <rockstarsam1234@gmail.com>
2021-07-23 17:14:24 +0530

    Added react codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+
+const GreetingsSubheading = () => {
+    return(
+        <p style={{
+            fontSize: "1.5em",
+            color: "wheat"
+        }}>
+            This is our first React App!
+        </p>
+    );
+}
+
+class Greetings extends React.Component{
+    render() {
+       return(
+           <p style={{
+               fontSize: "2em",
+               color: "white"
+           }}> Greetings! Welcome to our Site. </p>
+       )
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const SubHeading = () => {
+        return(
+            <p style={{
+                fontSize: "1em",
+                fontFamily: "monospace",
+                fontWeight: "bold",
+                color: "#61dafb"
+            }}>
+                This is second subheading!
+            </p>
+        )
+    }
+
+    return(
+        <div style={{
+            width: "100vw",
+            height: "100vh",
+            backgroundColor: "#2b2626",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <Greetings />
+            <GreetingsSubheading />
+            <SubHeading />
+        </div>
+    )
 }
+
 
 export default App;
